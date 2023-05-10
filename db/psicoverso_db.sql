914b2ec7ff6c10f339279a035ff4df176f2eaca2
Anderson Viana <92057959+andersonzero0@users.noreply.github.com>
2023-05-09 21:47:29 -0300

    Update psicoverso_db.sql

--- db/psicoverso_db.sql
+++ db/psicoverso_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 07, 2023 at 11:49 PM
+-- Generation Time: May 10, 2023 at 02:47 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,19 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users_tb` (
-  `id_user` int(11) NOT NULL,
-  `fullname` varchar(70) DEFAULT NULL,
-  `email` varchar(60) DEFAULT NULL,
-  `telefone` varchar(30) DEFAULT NULL,
-  `about_user` varchar(1000) DEFAULT NULL,
-  `type_user` varchar(15) DEFAULT NULL,
-  `birthDate` varchar(10) DEFAULT NULL,
-  `avatarURI` varchar(100) NOT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `street` varchar(100) DEFAULT NULL,
-  `square` varchar(100) DEFAULT NULL,
-  `uf` varchar(2) DEFAULT NULL,
-  `password_user` varchar(100) DEFAULT NULL
+  `id` int(11) NOT NULL,
+  `nome` varchar(24) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `senha` varchar(24) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -51,7 +42,7 @@ CREATE TABLE `users_tb` (
 -- Indexes for table `users_tb`
 --
 ALTER TABLE `users_tb`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -61,7 +52,7 @@ ALTER TABLE `users_tb`
 -- AUTO_INCREMENT for table `users_tb`
 --
 ALTER TABLE `users_tb`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
